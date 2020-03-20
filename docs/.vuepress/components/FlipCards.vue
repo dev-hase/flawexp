90668b914c344070d6c4c62466199914bb192f0e
devhase <meine.elektropost@web.de>
2020-03-20 08:52:01 +0100

    anchor manifest

---
+++ docs/.vuepress/components/FlipCards.vue
@@ -0,0 +1,32 @@
+<template>
+      <div class="flipcarddeck">
+            <header class="flipcardheader listview">
+                {{ titel }}
+                <button class="flipbtn" v-on:click="toggleView"> 
+                    ...zur Karteikartenansicht
+                </button>
+            </header>
+            <section class="carddeck listview">
+                <Content slot-key="cards"/>
+            </section>
+      </div>  
+</template>
+
+<script>
+export default {
+    props: ["titel"],
+    methods: {
+        toggleView: function() {
+
+            this.$el.querySelector('.carddeck').classList.toggle("listview");
+            this.$el.querySelector('.carddeck').classList.toggle("cardview");
+
+        }
+      }
+};
+</script>
+
+<style lang="stylus" scoped>
+
+
+</style>
